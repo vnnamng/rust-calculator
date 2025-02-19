9ba3faaa28be21ecda12bb5fb80121118f3acf23
vnnamng <e0969384@u.nus.edu>
2025-02-20 02:14:47 +0800

    clean up legacy code

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,5 @@
 mod big_num;
-mod big_num_legacy;
 mod frac;
-mod frac_legacy;
 mod parser;
-
-use crate::big_num::BigNum;
-use crate::frac_legacy::Frac;
-use std::env;
-use std::str::FromStr;
 
 fn main() {}
--- src/parser.rs
+++ src/parser.rs
@@ -1,5 +1,1 @@
-use std::str::FromStr;
-
-use crate::big_num_legacy::BigNum;
-use crate::frac_legacy::Frac;
 
