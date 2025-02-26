c20392cd9ffdd8d4ffc42f76ab518c90b5209d12
vnnamng <e0969384@u.nus.edu>
2025-02-26 19:20:33 +0800

    add eval_to_string for deploy

--- src/parser.rs
+++ src/parser.rs
@@ -262,6 +262,17 @@ pub fn eval(line: String) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+pub fn eval_to_string(input: String) -> Result<String, Box<dyn Error>> {
+    let tokens = lex(input)?;
+    let mut token_iter: Peekable<Iter<'_, Token>> = tokens.iter().peekable();
+    let mut parser = Parser::new(&mut token_iter);
+    let result = parser.parse();
+    match result {
+        Ok(mut ast) => Ok(format!("{}", ast.eval())),
+        Err(e) => return Err(Box::new(e)),
+    }
+}
+
 fn get_line() -> String {
     print!("> ");
     std::io::stdout().flush().unwrap();
